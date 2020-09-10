66adbe986262cfa8aac9c99451df92731b7b260e
Yakun Lyu <49229549+LUI24@users.noreply.github.com>
2020-09-10 22:47:43 +0800

    2020/09/10 daily study

---
+++ src/main/java/leetcode/p32/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.p32;
+
+import java.util.Stack;
+
+/**
+ * 返回有效括号的最大长度
+ */
+public class Solution {
+    public int longestValidParentheses(String s) {
+        if (s == null || s.length() < 2) {
+            return 0;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLength = Math.max(maxLength, i - stack.peek());
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
---
+++ src/main/java/leetcode/p92/Solution.java
@@ -0,0 +1,54 @@
+package leetcode.p92;
+
+import leetcode.entity.ListNode;
+
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy, next = dummy;
+        ListNode cur = head;
+
+        while (n >= 1) {
+            if (m == 2) {
+                pre = cur;
+            }
+            if (n == 1) {
+                next = cur.next;
+                break;
+            }
+            cur =  cur.next;
+            m--;
+            n--;
+        }
+
+        ListNode start = pre.next;
+        ListNode end = cur;
+
+        end.next = null;
+        pre.next = reverse(start);
+        start.next = next;
+
+        return dummy.next;
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = dummy.next;
+            dummy.next = head;
+            head = temp;
+        }
+
+        return dummy.next;
+    }
+}
